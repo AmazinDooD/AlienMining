544cecc6ff4a7679541ace7a359867378fd70973
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2025-11-13 20:27:24 +0000

    yup

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from "vite";
-export default defineConfig({ base: "./", })
+export default defineConfig({ base: "./AlienMining/", })
 
 // Somehow these two lines make building just... work. Fuck me.
